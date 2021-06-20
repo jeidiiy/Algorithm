2fc40c3459628839fa3d7632b8fffcd2be418fe0
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-06-20 18:24:11 +0900

    solve: 수들의 합

---
+++ baekjoon/greedy/BOJ1789.java
@@ -0,0 +1,27 @@
+package baekjoon.greedy;
+
+import java.io.*;
+
+class BOJ1789 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    long num = Long.parseLong(br.readLine());
+    long sum = 1;
+    long count = 1;
+
+    while (sum < num) {
+      count += 1;
+      sum += count;
+    }
+
+    if (sum > num) {
+      count -= 1;
+    }
+    bw.write(String.valueOf(count));
+
+    br.close();
+    bw.close();
+  }
+}
