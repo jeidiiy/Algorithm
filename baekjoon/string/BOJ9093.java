a1f823edd3dfc502d02248c991d103ad3adce996
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-12 19:42:07 +0900

    solve: 단어 뒤집기

---
+++ baekjoon/string/BOJ9093.java
@@ -0,0 +1,36 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ9093 {
+  public static void main(String[] argv) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int taskcase = Integer.parseInt(br.readLine().trim());
+
+    String input;
+    String[] tokens;
+
+    for (int i = 0; i < taskcase; i++) {
+      input = br.readLine().trim();
+      tokens = input.split(" ");
+
+      for (int j = 0; j < tokens.length; j++) {
+        String token = tokens[j];
+
+        for (int k = 0; k < token.length(); k++) {
+          bw.write(token.charAt(token.length() - (k + 1)));
+        }
+        bw.write(" ");
+      }
+      bw.write("\n");
+    }
+
+    br.close();
+    bw.close();
+  }
+}
