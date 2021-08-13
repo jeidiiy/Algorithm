7c32a8a61c6bb5fa9eb410c7e5f0fee224fd4531
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-13 21:50:24 +0900

    solve: 최소 힙

---
+++ baekjoon/priority_queue/BOJ1927.java
@@ -0,0 +1,34 @@
+package priority_queue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class BOJ1927 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    Queue<Integer> pq = new PriorityQueue<>();
+    StringBuffer sb = new StringBuffer();
+
+    int taskcase = Integer.parseInt(br.readLine().trim());
+
+    for (int i = 0; i < taskcase; i++) {
+      int input = Integer.parseInt(br.readLine().trim());
+
+      if (input == 0) {
+        sb.append(pq.isEmpty() ? 0 + "\n" : pq.poll() + "\n");
+      } else {
+        pq.offer(input);
+      }
+    }
+
+    bw.write(sb.toString());
+    br.close();
+    bw.close();
+  }
+}
