cb8953bb112229b7bad9ca0ed87a94244521f375
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-13 18:11:28 +0900

    solve: 카드2

---
+++ baekjoon/queue/BOJ2164.java
@@ -0,0 +1,119 @@
+package queue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2164 {
+  public static void main(String[] argv) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine().trim());
+    CustomQueue queue = new CustomQueue();
+
+    for (int i = 1; i <= N; i++) {
+      queue.push(i);
+    }
+
+    while(queue.size() != 1) {
+      // 버리는 카드
+      queue.pop();
+
+      int recycleCard = queue.pop();
+      queue.push(recycleCard);
+    }
+
+    bw.write(String.valueOf(queue.pop()));
+
+    br.close();
+    bw.close();
+  }
+
+  static class CustomQueue {
+    private Node front;
+    private Node rear;
+    private int numOfData;
+  
+    public void push(int data) {
+      Node newNode = new Node(data);
+      numOfData++;
+  
+      if (front == null) {
+        front = newNode;
+        rear = newNode;
+        return;
+      }
+  
+      rear.setNextNode(newNode);
+      rear = newNode;
+    }
+  
+    public int pop() {
+      int data;
+  
+      if (front != null) {
+        data = front.getData();
+        front = front.getNextNode();
+        numOfData--;
+      } else {
+        data = -1;
+        numOfData = 0;
+      }
+  
+      return data;
+    }
+  
+    public int size() {
+      return this.numOfData;
+    }
+  
+    public int empty() {
+      return size() == 0 ? 1 : 0;
+    }
+  
+    public int front() {
+      return size() == 0 ? -1 : front.getData();
+    }
+  
+    public int back() {
+      return size() == 0 ? -1 : rear.getData();
+    }
+  }
+  
+  static class Node {
+    private int data;
+    private Node nextNode;
+    private Node prevNode;
+  
+    public Node() {
+      data = -1;
+    }
+  
+    public Node(int data) {
+      this.data = data;
+    }
+  
+    public int getData() {
+      return this.data;
+    }
+  
+    public void setNextNode(Node nextNode) {
+      this.nextNode = nextNode;
+    }
+  
+    public Node getNextNode() {
+      return this.nextNode;
+    }
+  
+    public void setPrevNode(Node prevNode) {
+      this.prevNode = prevNode;
+    }
+  
+    public Node getPrevNode() {
+      return this.prevNode;
+    }
+  }
+}
+
