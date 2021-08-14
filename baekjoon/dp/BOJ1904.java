a0373686555a4f31eba1f9be8f8705bb9e8c2b4a
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-15 07:13:49 +0900

    solve: 01타일

---
+++ baekjoon/dp/BOJ1904.java
@@ -0,0 +1,32 @@
+package dp;
+
+import java.io.*;
+
+public class BOJ1904 {
+
+  static int[] dp;
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    dp = new int[N + 3];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    if (N == 1)
+      System.out.println(1);
+    else if (N == 2)
+      System.out.println(2);
+    else {
+      for (int i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+      }
+      System.out.println(dp[N]);
+    }
+
+    br.close();
+  }
+}
