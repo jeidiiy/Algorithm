c197c7e3d19abfc3475f07e389bf7cb98f25a686
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-20 22:18:27 +0900

    solve: 큰 수 A+B

---
+++ baekjoon/math/BOJ10757.java
@@ -0,0 +1,17 @@
+package math;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class BOJ10757 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] tokens = br.readLine().split(" ");
+
+    BigInteger a = new BigInteger(tokens[0]);
+    BigInteger b = new BigInteger(tokens[1]);
+
+    System.out.println(a.add(b));
+  }
+}
