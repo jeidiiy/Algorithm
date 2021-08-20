8ab129e7bfd6979ca9348fd1d03cd5c4a34f12cd
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-21 01:42:11 +0900

    solve: 직각삼각형

---
+++ baekjoon/math/BOJ4153.java
@@ -0,0 +1,38 @@
+package math;
+
+import java.io.*;
+
+public class BOJ4153 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String input;
+    String[] tokens;
+    int[] tri = new int[3];
+    while (true) {
+      input = br.readLine();
+      tokens = input.split(" ");
+
+      for (int i = 0; i < 3; i++) {
+        tri[i] = Integer.parseInt(tokens[i]);
+      }
+
+      if (tri[0] == 0 && tri[1] == 0 && tri[2] == 0) {
+        break;
+      }
+
+      if (tri[0] * tri[0] + tri[1] * tri[1] == tri[2] * tri[2]) {
+        System.out.println("right");
+      } else if (tri[0] * tri[0] + tri[2] * tri[2] == tri[1] * tri[1]) {
+        System.out.println("right");
+      } else if (tri[1] * tri[1] + tri[2] * tri[2] == tri[0] * tri[0]) {
+        System.out.println("right");
+      } else {
+        System.out.println("wrong");
+      }
+
+    }
+  }
+}
