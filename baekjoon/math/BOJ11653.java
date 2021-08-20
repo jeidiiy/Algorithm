2dbfb8704873a98441a5ed6e60586cd8f91165b0
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-21 00:49:52 +0900

    solve: 소인수분해

---
+++ baekjoon/math/BOJ11653.java
@@ -0,0 +1,33 @@
+package math;
+
+import java.io.*;
+
+public class BOJ11653 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+
+    int N = Integer.parseInt(br.readLine());
+
+    int n = 2;
+
+    while (true) {
+      if (N % n == 0) {
+        sb.append(n).append("\n");
+        N /= n;
+        continue;
+      }
+
+      if (N == 1) {
+        break;
+      }
+
+      n++;
+    }
+
+    System.out.print(sb);
+
+  }
+}
