f1a5bf310229dd8691d7f5d8a8eeee24464e3dce
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-22 05:23:47 +0900

    solve: 약수

---
+++ baekjoon/nt_com/BOJ1037.java
@@ -0,0 +1,24 @@
+package nt_com;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ1037 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    String input = br.readLine();
+    String[] tokens = input.split(" ");
+    int[] divisors = new int[tokens.length];
+
+    for (int i = 0; i < divisors.length; i++) {
+      divisors[i] = Integer.parseInt(tokens[i]);
+    }
+
+    Arrays.sort(divisors);
+
+    System.out.println(divisors[0] * divisors[divisors.length - 1]);
+  }
+}
