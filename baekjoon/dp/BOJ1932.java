d973b4643dca6ebcfeeba04e36574625d0b4c633
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-23 23:36:40 +0900

    solve: 정수 삼각형

---
+++ baekjoon/dp/BOJ1932.java
@@ -0,0 +1,47 @@
+package dp;
+
+import java.io.*;
+
+public class BOJ1932 {
+
+  static int[][] nums;
+  static Integer[][] dp;
+  static int n;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    n = Integer.parseInt(br.readLine());
+    nums = new int[n][n];
+    dp = new Integer[n][n];
+
+    String input;
+    String[] tokens;
+    for (int i = 0; i < n; i++) {
+      input = br.readLine();
+      tokens = input.split(" ");
+
+      for (int j = 0; j < i + 1; j++) {
+        nums[i][j] = Integer.parseInt(tokens[j]);
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      dp[n - 1][i] = nums[n - 1][i];
+    }
+
+    System.out.println(solution(0, 0));
+
+  }
+
+  static int solution(int depth, int idx) {
+    if (depth == n - 1)
+      return dp[depth][idx];
+
+    if (dp[depth][idx] == null) {
+      dp[depth][idx] = Math.max(solution(depth + 1, idx), solution(depth + 1, idx + 1)) + nums[depth][idx];
+    }
+
+    return dp[depth][idx];
+  }
+}
