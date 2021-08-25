1dfc1e85a6dce716be40a3523f2f5ccd6f67d211
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-25 23:08:38 +0900

    solve: N과 M (3)

---
+++ baekjoon/backtracking/BOJ15651.java
@@ -0,0 +1,43 @@
+package baekjoon.backtracking;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ15651 {
+
+  static int N;
+  static int M;
+  static int[] arr;
+  static StringBuffer sb = new StringBuffer();
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String input = br.readLine();
+    StringTokenizer st = new StringTokenizer(input, " ");
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[M];
+
+    progression(0);
+    System.out.println(sb);
+  }
+
+  static void progression(int depth) {
+    if (depth == M) {
+      for (int val : arr) {
+        sb.append(val).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+      arr[depth] = i;
+      progression(depth + 1);
+    }
+  }
+}
