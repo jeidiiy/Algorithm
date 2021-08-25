85899e717e329e9b76c1b45fd6b7da5586d495be
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-08-25 22:33:34 +0900

    solve: 큰 수 만들기

---
+++ programmers/greedy/MakeBigNumber.java
@@ -0,0 +1,23 @@
+package programmers.greedy;
+
+public class MakeBigNumber {
+
+  public String solution(String number, int k) {
+    StringBuilder answer = new StringBuilder();
+
+    int idx = 0;
+    int max = 0;
+
+    for (int i = 0; i < number.length() - k; i++) {
+      max = 0;
+      for (int j = idx; j <= i + k; j++) {
+        if (max < number.charAt(j) - '0') {
+          max = number.charAt(j) - '0';
+          idx = j + 1;
+        }
+      }
+      answer.append(max);
+    }
+    return answer.toString();
+  }
+}
