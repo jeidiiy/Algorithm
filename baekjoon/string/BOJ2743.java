ec7e76aa477f52c1258b9d4061cd55b637892504
jeidiiy <wldyd2356@gmail.com>
2022-05-19 21:03:49 +0900

    solve: 단어 길이 재기

---
+++ baekjoon/string/BOJ2743.java
@@ -0,0 +1,11 @@
+package baekjoon.string;
+
+import java.io.*;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(br.readLine().length());
+        }
+    }
+}
