7987fcf7f5f4dbdca7845fc6287fa7af9da94c4a
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2022-12-26 15:50:16 +0900

    solve: 과일 장수

---
+++ programmers/과일_장수.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+public class 과일_장수 {
+    class Solution {
+        public int solution(int k, int m, int[] score) {
+            int answer = 0;
+
+            // 이익이 발생하지 않는 경우
+            if (score.length < m)
+                return 0;
+
+            score = Arrays.stream(score).boxed().sorted(Collections.reverseOrder())
+                    .mapToInt(Integer::intValue).toArray();
+
+            for (int i = m; i <= score.length; i += m) {
+                answer += score[i - 1] * m;
+            }
+
+            return answer;
+        }
+    }
+
+}
