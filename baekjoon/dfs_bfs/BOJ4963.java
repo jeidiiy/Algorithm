9e2a7596ddf1faaf7dc47912d88be55f85f08d8d
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2023-02-14 13:18:24 +0900

    solve: 섬의 개수

---
+++ baekjoon/dfs_bfs/BOJ4963.java
@@ -0,0 +1,74 @@
+package baekjoon.dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ4963 {
+    static int[][] map;
+    // 상하좌우, 오른쪽윗대각, 오른쪽아랫대각, 왼쪽윗대각, 왼쪽아랫대각
+    static int[] dx = {-1, 1, 0, 0, -1, 1, -1, 1};
+    static int[] dy = {0, 0, -1, 1, 1, 1, -1, -1};
+    static boolean[][] isVisited;
+    static int width;
+    static int height;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String[] tokens = br.readLine().split(" ");
+                width = Integer.parseInt(tokens[0]);
+                height = Integer.parseInt(tokens[1]);
+                answer = 0;
+
+                if (width == 0 && height == 0)
+                    break;
+
+                map = new int[height + 1][width + 1];
+                isVisited = new boolean[height + 1][width + 1];
+
+                for (int h = 0; h < height; h++) {
+                    int[] inputs = Arrays.stream(br.readLine().split(" "))
+                            .mapToInt(Integer::parseInt).toArray();
+
+                    for (int w = 0; w < width; w++) {
+                        map[h][w] = inputs[w];
+                    }
+                }
+
+                for (int h = 0; h < height; h++) {
+                    for (int w = 0; w < width; w++) {
+                        if (!isVisited[h][w]) {
+                            if (dfs(h, w) == 1)
+                                answer++;
+                        }
+                    }
+                }
+
+                System.out.println(answer);
+            }
+        }
+    }
+
+    private static int dfs(int h, int w) {
+        isVisited[h][w] = true;
+        int currentLocation = map[h][w];
+
+        for (int i = 0; i < 8; i++) {
+            int nextH = h + dx[i];
+            int nextW = w + dy[i];
+
+            if (nextH < 0 || nextW < 0 || nextH > height || nextW > width) {
+                continue;
+            }
+
+            if (!isVisited[nextH][nextW] && map[nextH][nextW] == currentLocation) {
+                dfs(nextH, nextW);
+            }
+        }
+
+        return currentLocation;
+    }
+}
