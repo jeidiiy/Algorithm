64a537e7c80bca8c95daee2c8b94aa0928795a19
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2023-02-15 21:31:28 +0900

    feat: BFS 추가

--- baekjoon/dfs_bfs/BOJ2644.java
+++ baekjoon/dfs_bfs/BOJ2644.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BOJ2644 {
 
@@ -14,7 +16,7 @@ public class BOJ2644 {
     static ArrayList<Integer>[] tree;
     static boolean[] isVisited;
     static boolean isFamily = false;
-    static int answer;
+    static int answer = -1;
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
@@ -37,9 +39,10 @@ public class BOJ2644 {
                 tree[relationships[1]].add(relationships[0]);
             }
 
-            dfs(targets[0], 0);
+            // dfs(targets[0], 0);
+            bfs();
 
-            System.out.println(isFamily ? answer : -1);
+            System.out.println(answer);
         }
     }
 
@@ -47,7 +50,7 @@ public class BOJ2644 {
         isVisited[index] = true;
         if (index == targets[1]) {
             answer = count;
-            isFamily = true;
+            return;
         }
 
         for (int i : tree[index]) {
@@ -55,6 +58,52 @@ public class BOJ2644 {
                 isVisited[i] = true;
                 dfs(i, count + 1);
             }
+        }
+    }
+
+    private static void bfs() {
+        Queue<Tuple> queue = new LinkedList<Tuple>();
+        queue.add(Tuple.of(targets[0], 0));
+
+        while (!queue.isEmpty()) {
+            Tuple tuple = queue.remove();
+            int node = tuple.getNode();
+            int count = tuple.getCount();
+
+            if (node == targets[1]) {
+                answer = count;
+                break;
+            }
+
+            for (int i : tree[node]) {
+                if (!isVisited[i]) {
+                    isVisited[i] = true;
+                    queue.add(Tuple.of(i, count + 1));
+                }
+            }
+        }
+    }
+
+    private static class Tuple {
+
+        private final int node;
+        private final int count;
+
+        public Tuple(int node, int count) {
+            this.node = node;
+            this.count = count;
+        }
+
+        public static Tuple of(final int node, final int count) {
+            return new Tuple(node, count);
+        }
+
+        public int getNode() {
+            return node;
+        }
+
+        public int getCount() {
+            return count;
         }
     }
 }
