fda0bebda3ddac1f0d2b35dfff49bc6495dcc3f0
Jeidiiy <jeidiiy98@gmail.com>
2023-02-28 11:24:20 +0900

    refactor: DFS로만 해결
    메모리 향상: 134412KB -> 50648KB
    시간 단축 : 604ms -> 484ms

--- baekjoon/dfs_bfs/BOJ2573.java
+++ baekjoon/dfs_bfs/BOJ2573.java
@@ -2,9 +2,6 @@ package baekjoon.dfs_bfs;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class BOJ2573 {
 
     static int N, M;
@@ -18,8 +15,9 @@ public class BOJ2573 {
 
             N = Integer.parseInt(split[0]);
             M = Integer.parseInt(split[1]);
-
             map = new int[N][M];
+
+            boolean[][] isVisited;
 
             for (int i = 0; i < N; i++) {
                 int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
@@ -29,34 +27,32 @@ public class BOJ2573 {
                 }
             }
 
-            int ans = 0, cnt = 0;
-            while ((cnt = calcDividedBerg()) < 2) {
-                if (cnt == 0) {
-                    ans = 0;
-                    break;
+            int ans = 0;
+            Loop1: while (true) {
+                isVisited = new boolean[N][M];
+                boolean flag = false;
+                boolean allMelt = true;
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        if (map[i][j] > 0 && !isVisited[i][j]) {
+                            if (flag)
+                                break Loop1;
+                            else
+                                flag = true;
+                            allMelt = false;
+                            dfs(i, j, isVisited);
+                        }
+                    }
                 }
-                bfs();
+                if (allMelt) {
+                    System.out.println(0);
+                    return;
+                }
                 ans++;
             }
 
             System.out.println(ans);
         }
-    }
-
-    private static int calcDividedBerg() {
-        boolean[][] isVisited = new boolean[N][M];
-        int cnt = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != 0 && !isVisited[i][j]) {
-                    dfs(i, j, isVisited);
-                    cnt++;
-                }
-            }
-        }
-
-        return cnt;
     }
 
     private static void dfs(int x, int y, boolean[][] isVisited) {
@@ -72,59 +68,19 @@ public class BOJ2573 {
             if (map[nextX][nextY] > 0 && !isVisited[nextX][nextY])
                 dfs(nextX, nextY, isVisited);
         }
-    }
 
-    private static void bfs() {
-        Queue<Pos> queue = new LinkedList<>();
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
 
-        boolean[][] isVisited = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] > 0) {
-                    queue.add(new Pos(i, j));
-                    isVisited[i][j] = true;
-                }
-            }
+            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M)
+                continue;
+
+            if (map[nextX][nextY] == 0 && !isVisited[nextX][nextY])
+                map[x][y]--;
         }
 
-        while (!queue.isEmpty()) {
-            Pos pos = queue.remove();
-
-            int seaCnt = 0;
-
-            for (int i = 0; i < 4; i++) {
-                int adjX = pos.getX() + dx[i];
-                int adjY = pos.getY() + dy[i];
-
-                if (adjX < 0 || adjY < 0 || adjX >= N || adjY >= M)
-                    continue;
-
-                if (!isVisited[adjX][adjY] && map[adjX][adjY] <= 0)
-                    seaCnt++;
-            }
-
-            if (map[pos.getX()][pos.getY()] - seaCnt < 0)
-                map[pos.getX()][pos.getY()] = 0;
-            else
-                map[pos.getX()][pos.getY()] -= seaCnt;
-        }
-    }
-
-    static class Pos {
-        private int x;
-        private int y;
-
-        public Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
+        if (map[x][y] < 0)
+            map[x][y] = 0;
     }
 }
