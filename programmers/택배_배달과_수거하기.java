756f410a0c1d38c0bc4eae6dd9a0ef252d462311
Jeidiiy <jeidiiy98@gmail.com>
2023-05-02 10:40:04 +0900

    solve: 택배 배달과 수거하기

---
+++ programmers/택배_배달과_수거하기.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class 택배_배달과_수거하기 {
+    class Solution {
+        public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+            int deliver = 0, pickup = 0;
+            long answer = 0;
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (deliveries[i] != 0 || pickups[i] != 0) {
+                    int cnt = 0;
+                    while (deliver < deliveries[i] || pickup < pickups[i]) {
+                        cnt++;
+                        deliver += cap;
+                        pickup += cap;
+                    }
+                    deliver -= deliveries[i];
+                    pickup -= pickups[i];
+                    answer += (i + 1) * cnt * 2;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
