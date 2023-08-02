9dca2b8c1726606051682bf3722de914310dfa98
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-02 15:44:47 +0900

    refactor: 연,월,일을 구분하지 않고 일 형식으로 변경하여 해결

--- programmers/개인정보_수집_유효기간.java
+++ programmers/개인정보_수집_유효기간.java
@@ -6,66 +6,44 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class 개인정보_수집_유효기간 {
-
     class Solution {
+
+        private static final int DAY_OF_MONTH = 28;
+
         public int[] solution(String today, String[] terms, String[] privacies) {
             List<Integer> answer = new ArrayList<>();
             Map<String, Integer> termsMap = new HashMap<>();
+            int todayDay = getDay(today);
 
             for (String term : terms) {
                 String[] termDetails = term.split(" ");
+                String kindOfTerm = termDetails[0];
+                Integer period = Integer.valueOf(termDetails[1]);
 
-                termsMap.put(termDetails[0], Integer.valueOf(termDetails[1]));
+                termsMap.put(kindOfTerm, period);
             }
 
             int index = 0;
             for (String privacy : privacies) {
                 index++;
                 String[] privacyDetails = privacy.split(" ");
-                String collectDate = privacyDetails[0];
-
-                String[] dateDetails = collectDate.split("\\.");
-                int year = Integer.valueOf(dateDetails[0]);
-                int month = Integer.valueOf(dateDetails[1]);
-                int day = Integer.valueOf(dateDetails[2]);
-
+                String expireDate = privacyDetails[0];
                 String kindOfTerm = privacyDetails[1];
 
-                int validTerm = termsMap.get(kindOfTerm);
-
-                month += validTerm;
-
-                if (month >= 13) {
-                    if (month % 12 == 0) {
-                        year = year + (month / 12) - 1;
-                        month = 12;
-                    } else {
-                        year = year + (month / 12);
-                        month = month - 12 * (month / 12);
-                    }
+                if (getDay(expireDate) + (termsMap.get(kindOfTerm) * DAY_OF_MONTH) <= todayDay) {
+                    answer.add(index);
                 }
-
-                String[] todayDetails = today.split("\\.");
-                int todayYear = Integer.valueOf(todayDetails[0]);
-                int todayMonth = Integer.valueOf(todayDetails[1]);
-                int todayDay = Integer.valueOf(todayDetails[2]);
-
-                if (todayYear < year) {
-                    continue;
-                } else if (todayYear == year) {
-                    if (todayMonth < month) {
-                        continue;
-                    } else if (todayMonth == month) {
-                        if (todayDay < day) {
-                            continue;
-                        }
-                    }
-                }
-
-                answer.add(index);
             }
 
             return answer.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        private int getDay(String date) {
+            String[] dateDetails = date.split("\\.");
+            int year = Integer.parseInt(dateDetails[0]);
+            int month = Integer.parseInt(dateDetails[1]);
+            int day = Integer.parseInt(dateDetails[2]);
+            return (year * 12 * DAY_OF_MONTH) + (month * DAY_OF_MONTH) + day;
         }
     }
 }
