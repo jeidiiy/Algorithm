19b5d749ae7821964094abfeb30925936c4207b5
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-03 13:55:14 +0900

    solve: 성격 유형 검사하기

---
+++ programmers/성격_유형_검사하기.java
@@ -0,0 +1,46 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class 성격_유형_검사하기 {
+    class Solution {
+        public String solution(String[] survey, int[] choices) {
+            Map<String, Integer> personalityType = new HashMap<>();
+
+            init(personalityType);
+
+            for (int i = 0; i < survey.length; i++) {
+                String[] types = survey[i].split("");
+                int choice = choices[i];
+
+                int result = choice - 4;
+                int selectedType = result < 0 ? 0 : 1;
+
+                if (result != 0) {
+                    personalityType.put(
+                            types[selectedType],
+                            personalityType.get(types[selectedType]) + Math.abs(result));
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(personalityType.get("R") >= personalityType.get("T") ? "R" : "T");
+            sb.append(personalityType.get("C") >= personalityType.get("F") ? "C" : "F");
+            sb.append(personalityType.get("J") >= personalityType.get("M") ? "J" : "M");
+            sb.append(personalityType.get("A") >= personalityType.get("N") ? "A" : "N");
+
+            return sb.toString();
+        }
+
+        private void init(Map<String, Integer> map) {
+            map.put("R", 0);
+            map.put("T", 0);
+            map.put("C", 0);
+            map.put("F", 0);
+            map.put("J", 0);
+            map.put("M", 0);
+            map.put("A", 0);
+            map.put("N", 0);
+        }
+    }
+}
