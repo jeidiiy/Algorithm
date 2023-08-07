3d1a25b93ff9f21a0a245f11c460edd061caa516
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-07 22:55:11 +0900

    refactor: Integer.bitCount 적용

--- programmers/다음_큰_숫자.java
+++ programmers/다음_큰_숫자.java
@@ -4,24 +4,13 @@ public class 다음_큰_숫자 {
     class Solution {
         public int solution(int n) {
             int nextBigNumber = n;
-            int onecountOfBinaryN = countBinaryOne(Integer.toBinaryString(n));
+            int onecountOfBinaryN = Integer.bitCount(n);
             while (true) {
                 nextBigNumber++;
-                if (countBinaryOne(Integer.toBinaryString(nextBigNumber)) == onecountOfBinaryN)
+                if (Integer.bitCount(nextBigNumber) == onecountOfBinaryN)
                     break;
             }
             return nextBigNumber;
-        }
-
-        private int countBinaryOne(String s) {
-            int result = 0;
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '1')
-                    result++;
-            }
-
-            return result;
         }
     }
 }
