c37353c827ed164323871ce745da12fd95f8dd4f
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-07 19:03:14 +0900

    solve: 이진 변환 반복하기

---
+++ programmers/이진_변환_반복하기.java
@@ -0,0 +1,45 @@
+package programmers;
+
+public class 이진_변환_반복하기 {
+    class Solution {
+        private int count = 0;
+        private int zeros = 0;
+
+        public int[] solution(String s) {
+            convertToBinary(s);
+
+            return new int[] { count, zeros };
+        }
+
+        private void convertToBinary(String s) {
+            if (s.equals("1")) {
+                return;
+            }
+
+            count++;
+
+            int curZeros = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+
+                if (ch == '0') {
+                    curZeros++;
+                    continue;
+                }
+            }
+            zeros += curZeros;
+
+            int len = s.length() - curZeros;
+
+            StringBuilder sb = new StringBuilder();
+            while (len > 1) {
+                sb.append(len % 2);
+                len = len / 2;
+            }
+            sb.append(1);
+            String next = sb.reverse().toString();
+
+            convertToBinary(next);
+        }
+    }
+}
