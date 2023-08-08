e4af47e5550601eeac969057498f1becf2495238
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-08 14:25:58 +0900

    solve: N개의 최소공배수

---
+++ programmers/N개의_최소공배수.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class N개의_최소공배수 {
+    class Solution {
+        public int solution(int[] arr) {
+            int answer = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                answer = lcm(answer, arr[i]);
+            }
+
+            return answer;
+        }
+
+        public int gcd(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+
+        public int lcm(int a, int b) {
+            return (a * b) / gcd(a, b);
+        }
+    }
+}
