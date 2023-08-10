1e3d2f25b044d2357bbace0e765b49c6ca0ac1f7
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-10 15:31:45 +0900

    fix: row col 변경

--- programmers/n2_배열_자르기.java
+++ programmers/n2_배열_자르기.java
@@ -6,9 +6,9 @@ public class n2_배열_자르기 {
             int[] result = new int[(int) (right - left) + 1];
 
             for (int i = 0; left <= right; i++, left++) {
-                int col = (int) (left / n + 1);
-                int row = (int) (left % n + 1);
-                result[i] = Math.max(col, row);
+                int row = (int) (left / n + 1);
+                int col = (int) (left % n + 1);
+                result[i] = Math.max(row, col);
             }
 
             return result;
