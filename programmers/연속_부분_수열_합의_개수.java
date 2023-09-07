b4929b42e3191529272578ddfa3e85048cc2684b
JIYONG JUNG <jeidiiy98@gmail.com>
2023-09-07 19:30:50 +0900

    refactor: 배열을 늘리는 방법으로 시간복잡도 개선

--- programmers/연속_부분_수열_합의_개수.java
+++ programmers/연속_부분_수열_합의_개수.java
@@ -6,14 +6,18 @@ import java.util.HashSet;
 public class 연속_부분_수열_합의_개수 {
     class Solution {
         public int solution(int[] elements) {
+            int[] newElements = new int[elements.length * 2];
+
+            for (int i = 0; i < elements.length; i++) {
+                newElements[i] = newElements[i + elements.length] = elements[i];
+            }
+
             Set<Integer> set = new HashSet<>();
 
-            for (int i = 1; i <= elements.length; i++) {
-                for (int j = 0; j < elements.length; j++) {
-                    int sum = 0;
-                    for (int k = j; k < j + i; k++) {
-                        sum += elements[k % elements.length];
-                    }
+            for (int i = 0; i < elements.length; i++) {
+                int sum = 0;
+                for (int k = i; k < i + elements.length; k++) {
+                    sum += newElements[k];
                     set.add(sum);
                 }
             }
